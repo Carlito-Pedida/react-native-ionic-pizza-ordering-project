2d333b05b9d34b52c9aef971f787431221521b2b
ionic-project <carlped417@gmail.com>
2023-12-30 21:26:44 -0600

    project init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'l02-assignment',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
